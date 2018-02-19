a5e444a844ea1318dccfe89335046ba19053e05d
Yang Zhou <zhouyang@elastos.org>
2018-02-20 00:35:03 +0800

    add oauth login

--- resources/assets/js/components/common/TopMenu.vue
+++ resources/assets/js/components/common/TopMenu.vue
@@ -7,10 +7,13 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
             <router-link to="/about" tag="li">
-                <a>关 于</a>
+                <a>关于</a>
             </router-link>
             <li>
-                <router-link to="/register">注 册</router-link>
+                <router-link to="/login">登录</router-link>
+            </li>
+            <li>  
+                <router-link to="/register">注册</router-link>
             </li>
             </ul>
         </div>
